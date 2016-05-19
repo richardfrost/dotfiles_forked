95f05fe3746e65ba3a0fc76edd332a9eed4d156f
Richard Frost <richardfrost@users.noreply.github.com>
2016-05-19 15:08:49 -0600

    Add Atom stars to installation script

---
+++ atom.symlink/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Atom Stars allows you to keep track of your favorite packages
+# To star a package:
+#   apm star package-name
+# To star all installed packages:
+#   apm star --installed
+
+# Install starred packages
+apm stars --install
