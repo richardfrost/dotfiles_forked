874abadde8a2488908e31081ebf778d6b322a745
Richard Frost <richardfrost@users.noreply.github.com>
2016-05-31 11:32:36 -0600

    path fixes and zsh rbenv

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,4 +2,4 @@ export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Path For Oh-my-zsh
-export PATH="/usr/local/var/rbenv/shims:/usr/local/opt/coreutils/libexec/gnubin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export PATH="/usr/local/var/rbenv/shims:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
