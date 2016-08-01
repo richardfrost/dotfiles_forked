d7f1a9dc02393e321f6705c7712cf4dd2f236780
Richard Frost <richardfrost@users.noreply.github.com>
2016-08-01 16:29:32 -0600

    Change osx default for shorter bannerTime

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -45,3 +45,6 @@ defaults write com.apple.dock wvous-bl-modifier -int 0
 # defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 # defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 # defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Hide the notification faster
+defaults write com.apple.notificationcenterui bannerTime 2
