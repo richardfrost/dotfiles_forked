2aee46e837d0f6ea8c9e3f067b57471cb7c4856d
Richard Frost <richardfrost@users.noreply.github.com>
2016-08-13 11:27:11 -0600

    Adding support for zsh host completion in ssh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,21 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+###
+# Hostname completion for ssh commands
+$h=()
+
+# Compltion from history
+# h=($(echo $(history | awk '{print $2 " " $3}' | grep 'ssh ' | awk '{print $2}' | sort -u)))
+
+# Completion from custom_hosts list: "$HOME/.ssh/custom_hosts"
+[[ -r $HOME/.ssh/custom_hosts ]] && h=($h $(cat $HOME/.ssh/custom_hosts))
+
+if [[ $#h -gt 0 ]]; then
+  zstyle ':completion:*:ssh:*' hosts $h
+fi
+
+###
+# git
+# compdef '_dispatch git git' g
