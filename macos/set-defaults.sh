e1fc711712375fc67796f602e8324e989bce85dd
Richard Frost <richardfrost@users.noreply.github.com>
2017-01-13 13:31:19 -0700

    Merge upstream

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,7 +1,5 @@
 # Sets reasonable macOS defaults.
 #
-# Or, in other words, set shit how I like in macOS.
-#
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 #
@@ -37,15 +35,15 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
-# # Hide Safari's bookmark bar.
-# defaults write com.apple.Safari ShowFavoritesBar -bool false
-#
-# # Set up Safari for development.
-# defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-# defaults write com.apple.Safari IncludeDevelopMenu -bool true
-# defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-# defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-# defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+# Hide Safari's bookmark bar.
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 # Hide the notification faster
 defaults write com.apple.notificationcenterui bannerTime 2
@@ -54,7 +52,7 @@ defaults write com.apple.notificationcenterui bannerTime 2
 defaults write com.apple.Dock autohide-delay -float 0
 
 # Enable text selection for Quick Look
-defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+defaults write com.apple.finder QLEnableTextSelection -bool true
 
 # Show system info on login screen by clicking clock
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
