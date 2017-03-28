cf50a601610abb46e2bac391e51beeb4da5a3a4b
Richard Frost <richardfrost@users.noreply.github.com>
2017-03-28 12:11:05 -0600

    Cleanup path variables

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,5 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="./bin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-# Path For Oh-my-zsh
-export PATH="/usr/local/var/rbenv/shims:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
